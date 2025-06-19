76af55ab760fe284ce6170f66238aede3641430f
Vaidehi Chavan <vc184000@gmail.com>
2025-06-19 22:20:04 +0530

    Add Week_1 folder

---
+++ Week_1/Week 1_Design principles & Patterns/1_Implementing the Factory Method Pattern/Code/WordDocument.java
@@ -0,0 +1,8 @@
+public class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening a Word document.");
+    }
+    public void close() {
+        System.out.println("Closing the Word document.");
+    }
+}
