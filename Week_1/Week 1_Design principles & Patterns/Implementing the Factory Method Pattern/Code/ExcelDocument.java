f891061e59d98bbb8368c658927cc9887c827fe8
chavanvaidehii <159686767+chavanvaidehii@users.noreply.github.com>
2025-06-19 22:34:55 +0530

    Delete Week_1 directory

--- Week_1/Week 1_Design principles & Patterns/Implementing the Factory Method Pattern/Code/ExcelDocument.java
+++
@@ -1,8 +0,0 @@
-public class ExcelDocument implements Document {
-    public void open() {
-        System.out.println("Opening an Excel document.");
-    }
-    public void close() {
-        System.out.println("Closing the Excel document.");
-    }
-}
--- Week_1/Week 1_Design principles & Patterns/Implementing the Factory Method Pattern/Code/PdfDocument.java
+++
@@ -1,8 +0,0 @@
-public class PdfDocument implements Document {
-    public void open() {
-        System.out.println("Opening a PDF document.");
-    }
-    public void close() {
-        System.out.println("Closing the PDF document.");
-    }
-}
