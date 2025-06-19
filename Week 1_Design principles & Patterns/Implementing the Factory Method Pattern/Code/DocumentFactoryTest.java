82c480d643b9b6432c4f9975a5e3419697e7b6c6
vaidehi <vaidehichavan43@gmail.com>
2025-06-19 22:55:25 +0530

    Initial commit

---
+++ Week 1_Design principles & Patterns/Implementing the Factory Method Pattern/Code/DocumentFactoryTest.java
@@ -0,0 +1,19 @@
+
+public class DocumentFactoryTest {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document word = wordFactory.createDocument();
+        word.open();
+        word.close();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdf = pdfFactory.createDocument();
+        pdf.open();
+        pdf.close();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excel = excelFactory.createDocument();
+        excel.open();
+        excel.close();
+    }
+}
---
+++ Week 1_Design principles & Patterns/Implementing the Factory Method Pattern/Code/PdfDocument.java
@@ -0,0 +1,8 @@
+public class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening a PDF document.");
+    }
+    public void close() {
+        System.out.println("Closing the PDF document.");
+    }
+}
